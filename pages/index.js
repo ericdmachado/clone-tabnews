3d3e619263c7b186af3a630ae316d3acdb5f8dde
Eric Machado <ericdmachado2@gmail.com>
2025-02-02 15:14:32 -0300

    adicionando componentes da vercel

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
-  <SpeedInsights />;
-  return <h1>Estou aqui para ajudar e codar!!</h1>;
+  return (
+    <>
+      <SpeedInsights />
+      <h1>Estou aqui para ajudar e codar!!</h1>
+    </>
+  );
 }
