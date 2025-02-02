247e126e3d93549e17101d92952bfd2671185f36
Eric Machado <ericdmachado2@gmail.com>
2025-02-02 13:44:05 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-export default function Home(){
-  return (
-    <h1>Estou aqui para ajudar e codar!!</h1>
-  );
-};
+export default function Home() {
+  return <h1>Estou aqui para ajudar e codar!!</h1>;
+}
